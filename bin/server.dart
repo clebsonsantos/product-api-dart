215dd90ec84db7a7d9e33bb19dea9a84c27bc7ec
Clebson Santos <clebsonsantosmtt@gmail.com>
2022-12-23 14:33:45 -0300

    chore: prepare connection with database mysql]

--- bin/server.dart
+++ bin/server.dart
@@ -1,8 +1,10 @@
 import 'package:shelf_router/shelf_router.dart';
 import 'main.dart';
-
+import 'package:mysql1/mysql1.dart';
 final _router = Router();
 
-void main(List<String> args) async {
+void main() async {
+  var connection = await MySqlConnection.connect(ConnectionSettings(host: "localhost", port: 3306, user: "root", password: "docker", useSSL: false, db: "products_dart"));
+  print(connection);
   await bootstrap(_router);
 }
